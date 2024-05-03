16310684ec6ccc07e99118291c45475ce4ac6339
alvarado0925 <alvarado0925@gmail.com>
2024-05-02 21:54:28 -0500

    Conecta el Client con Products microservice

--- src/products/products.module.ts
+++ src/products/products.module.ts
@@ -1,9 +1,24 @@
 import { Module } from '@nestjs/common';
+import { ClientsModule, Transport } from '@nestjs/microservices';
 
+import { envs } from 'src/config';
+import { Services } from 'src/config/services.enum';
 import { ProductsController } from './products.controller';
 
 @Module({
   controllers: [ProductsController],
   providers: [],
+  imports: [
+    ClientsModule.register([
+      {
+        name: Services.PRODUCT,
+        transport: Transport.TCP,
+        options: {
+          host: envs.microservices.products.host,
+          port: envs.microservices.products.port,
+        },
+      },
+    ]),
+  ],
 })
 export class ProductsModule {}
