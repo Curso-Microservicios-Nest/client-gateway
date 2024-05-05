3260c69aae249412f48dd0b29daeb85030cf0a96
alvarado0925 <alvarado0925@gmail.com>
2024-05-04 22:57:24 -0500

    Se conectan los servicios faltantes entre Client y Product Microservice

--- src/products/dto/update-product.dto.ts
+++ src/products/dto/update-product.dto.ts
@@ -1,11 +1,5 @@
 import { PartialType } from '@nestjs/swagger';
-import { IsNotEmpty, IsNumber, IsPositive } from 'class-validator';
 
 import { CreateProductDto } from './create-product.dto';
 
-export class UpdateProductDto extends PartialType(CreateProductDto) {
-  @IsPositive()
-  @IsNumber()
-  @IsNotEmpty()
-  readonly id: number;
-}
+export class UpdateProductDto extends PartialType(CreateProductDto) {}
--- src/products/products.controller.ts
+++ src/products/products.controller.ts
@@ -1,4 +1,5 @@
 import {
+  Body,
   Controller,
   Delete,
   Get,
@@ -11,9 +12,11 @@ import {
 } from '@nestjs/common';
 import { ClientProxy, RpcException } from '@nestjs/microservices';
 import { ApiCreatedResponse, ApiOperation, ApiTags } from '@nestjs/swagger';
-import { firstValueFrom } from 'rxjs';
+import { catchError, firstValueFrom } from 'rxjs';
 import { PaginationDto } from 'src/common/dto/pagination.dto';
 import { Services } from 'src/enums/services.enum';
+import { CreateProductDto } from './dto/create-product.dto';
+import { UpdateProductDto } from './dto/update-product.dto';
 
 @Controller('products')
 @ApiTags('Products')
@@ -25,8 +28,8 @@ export class ProductsController {
   @Post()
   @ApiOperation({ summary: 'Create a new product' })
   @ApiCreatedResponse({ description: 'Product created successfully' })
-  create() {
-    return 'This action adds a new product';
+  async create(@Body() createProduct: CreateProductDto) {
+    return this.productsClient.send({ cmd: 'create' }, createProduct);
   }
 
   @Get()
@@ -49,19 +52,36 @@ export class ProductsController {
 
   @Patch(':id')
   @ApiOperation({ summary: 'Update a product by ID' })
-  update(@Param('id', ParseIntPipe) id: number) {
-    return `This action updates a #${id} product ${id}`;
+  async update(
+    @Param('id', ParseIntPipe) id: number,
+    @Body() updateProduct: UpdateProductDto,
+  ) {
+    try {
+      return await firstValueFrom(
+        this.productsClient.send({ cmd: 'update' }, { id, ...updateProduct }),
+      );
+    } catch (error) {
+      throw new RpcException(error);
+    }
   }
 
   @Delete(':id')
   @ApiOperation({ summary: 'Delete a product by ID' })
   remove(@Param('id', ParseIntPipe) id: number) {
-    return `This action removes a #${id} product ${id}`;
+    return this.productsClient.send({ cmd: 'remove' }, { id }).pipe(
+      catchError((err) => {
+        throw new RpcException(err);
+      }),
+    );
   }
 
   @Delete('soft-delete/:id')
   @ApiOperation({ summary: 'Soft delete a product by ID' })
   softRemove(@Param('id', ParseIntPipe) id: number) {
-    return `This action soft removes a #${id} product ${id}`;
+    return this.productsClient.send({ cmd: 'softRemove' }, { id }).pipe(
+      catchError((err) => {
+        throw new RpcException(err);
+      }),
+    );
   }
 }
