180c063bfbc6ebdaa37fb60cf2e2f484b949e002
alvarado0925 <alvarado0925@gmail.com>
2024-05-06 23:46:07 -0500

    Configura CreateOrderDto

--- src/orders/dto/create-order.dto.ts
+++ src/orders/dto/create-order.dto.ts
@@ -1,1 +1,36 @@
-export class CreateOrderDto {}
+import { ApiProperty } from '@nestjs/swagger';
+import {
+  IsBoolean,
+  IsEnum,
+  IsNotEmpty,
+  IsNumber,
+  IsOptional,
+  IsPositive,
+} from 'class-validator';
+import { OrderStatus } from '../enums/order.enum';
+
+export class CreateOrderDto {
+  @ApiProperty({ description: 'Id of the user', example: 1000 })
+  @IsPositive()
+  @IsNumber()
+  @IsNotEmpty()
+  readonly totalAmount: number;
+
+  @ApiProperty({ description: 'Total items in the order', example: 3 })
+  @IsPositive()
+  @IsNumber()
+  @IsNotEmpty()
+  readonly totalItems: number;
+
+  @ApiProperty({ description: 'Status of the order', example: 'PENDING' })
+  @IsEnum(OrderStatus, {
+    message: `Status must be one of the following: ${OrderStatus}`,
+  })
+  @IsOptional()
+  readonly status: OrderStatus = OrderStatus.PENDING;
+
+  @ApiProperty({ description: 'Paid status of the order', example: 'false' })
+  @IsBoolean()
+  @IsOptional()
+  readonly paid: boolean = false;
+}
